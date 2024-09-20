adfe3b70879cbb789a9eb2ea9932e73bec61f529
kolikoo <nikushasulxanishvili@gmail.com>
2024-09-20 18:15:57 +0400

    update 2

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,12 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import headerLogo from '../images/logo_image.png'
+import headerLogo from '../src/images/logo_image.png'
 import { country } from "./myScript";
 
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
