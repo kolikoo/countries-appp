da1ee4971ec5af9643db08d3befddd00c04f7b5f
kolikoo <nikushasulxanishvili@gmail.com>
2024-10-17 13:42:10 +0400

    homework 9

--- src/pages/home/components/homeArticle/components/articleContent/components/articleCreateForm/articleCreateForm.tsx
+++ src/pages/home/components/homeArticle/components/articleContent/components/articleCreateForm/articleCreateForm.tsx
@@ -1,15 +1,53 @@
-import React, { FormEvent } from "react";
+import React, { FormEvent, useState } from "react";
 import style from "./articleCreateForm.module.css";
 
 type ArticleCreateFormProps = {
   onArticleCreate: (event: FormEvent<HTMLFormElement>) => void;
+  title: string;
+  description: string;
+  onTitleChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  onDescriptionChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
 };
 
 const ArticleCreateForm: React.FC<ArticleCreateFormProps> = ({
   onArticleCreate,
+  title,
+  description,
+  onTitleChange,
+  onDescriptionChange,
 }) => {
+  const [titleError, setTitleError] = useState("");
+  const [descriptionError, setDescriptionError] = useState("");
+
+  const validateForm = () => {
+    let isValid = true;
+
+    if (title.length > 7) {
+      setTitleError("მაქსიმუმ 7 სიმბოლო უნდა იყოს");
+      isValid = false;
+    } else {
+      setTitleError("");
+    }
+
+    if (description.length < 3) {
+      setDescriptionError("მინიმუმ სამი სიმბოლო უნდა იყოს");
+      isValid = false;
+    } else {
+      setDescriptionError("");
+    }
+
+    return isValid;
+  };
+
+  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    if (validateForm()) {
+      onArticleCreate(event);
+    }
+  };
+
   return (
-    <form className={style.form} onSubmit={onArticleCreate}>
+    <form className={style.form} onSubmit={handleSubmit}>
       <div className={style.contact_fields}>
         <label htmlFor="title">title:</label>
         <input
@@ -17,8 +55,11 @@ const ArticleCreateForm: React.FC<ArticleCreateFormProps> = ({
           id="title"
           name="title"
           placeholder="enter article title"
+          value={title}
+          onChange={onTitleChange}
           required
         />
+        {titleError && <span className={style.error}>{titleError}</span>}
       </div>
 
       <div className={style.contact_fields}>
@@ -27,9 +68,14 @@ const ArticleCreateForm: React.FC<ArticleCreateFormProps> = ({
           type="text"
           id="description"
           name="description"
-          placeholder="writex the description"
+          placeholder="write the description"
+          value={description}
+          onChange={onDescriptionChange}
           required
         />
+        {descriptionError && (
+          <span className={style.error}>{descriptionError}</span>
+        )}
       </div>
 
       <button className={style.contact_button} type="submit">
